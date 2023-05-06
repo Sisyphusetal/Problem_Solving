4f9ad58d31f25a586215ff9004c2d4993c4a6fd4
Kurt James Clausen <kurtjclausen@gmail.com>
2023-05-06 13:15:20 -0700

    Problem_Solving

---
+++ problemSolving.js
@@ -0,0 +1,19 @@
+/*Prompt: A computer application needs to be able to identify if a written paragraph is 
+written in First, Second, or Third Person.*/
+
+/*One strategy to use this program would be to write the paragraph to a text file and
+then assing it to an array, then traverse the array to search for keywords indicating
+perspective the paragraph is written in.*/
+
+//First the paragraph would be made into a .txt file and then linked to an array
+//Then a for loop should traverse the array
+        //Ideally it could look for keywords like "me", "I", "his/he", "hers/her"
+        //Likely achieved through an if statement
+//Add to separate counts indicating the frequency of either type of language
+//Highest count should then reference what perspective it is likely to be in
+
+/*The most difficult part of problem solving is identifying what the actual problem is.
+If the general concept of the program is incorrectly or too vaguely conveyed in the beginning,
+then the problem solving process is flawed from its conception, and so it is likely that any
+solution that follows will not actually solve the problem, but instead solve whatever was
+erroneously posed as the problem in the beginning.*/
